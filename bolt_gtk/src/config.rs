07aedd0ebe5dbaba26cd56e019c906d0e67e2c8f
Kent Delante <leftybournes@pm.me>
2024-02-28 19:22:53 +0800

    Remove commited config file

--- bolt_gtk/src/config.rs
+++
@@ -1,8 +0,0 @@
-pub static VERSION: &str = "0.1.0";
-pub static GETTEXT_PACKAGE: &str = "bolt";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/bolt";
-pub static USER_AGENT: &str = "Bolt/1.0";
-pub static BASE_URL: &str = "https://api.podcastindex.org/api/1.0";
-pub static API_KEY: &str = "V3RXUCWU3MT26VAQ9WP9";
-pub static API_SECRET: &str = "zE6x6C$H7gza48#gM8VDmFApffH$Fh5pLygDk6DJ";
