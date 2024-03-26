4f6f1e981f6549c03256ea46694eda29cbd6f2d4
Kent Delante <leftybournes@pm.me>
2024-03-26 17:02:04 +0800

    Format flake

--- bolt_gtk/flake.nix
+++ bolt_gtk/flake.nix
@@ -6,36 +6,71 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages = rec {
-        bolt = pkgs.stdenv.mkDerivation {
-          pname = "bolt";
-          version = "0.1.0";
-          src = ./.;
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages = rec {
+          bolt = pkgs.stdenv.mkDerivation {
+            pname = "bolt";
+            version = "0.1.0";
+            src = ./.;
 
-          cargoDeps = pkgs.rustPlatform.importCargoLock {
-            lockFile = ./Cargo.lock;
+            cargoDeps =
+              pkgs.rustPlatform.importCargoLock { lockFile = ./Cargo.lock; };
+
+            buildInputs = with pkgs; [ gtk4 libadwaita sqlite ];
+
+            nativeBuildInputs = with pkgs; [
+              cargo
+              rustPlatform.cargoSetupHook
+              rustc
+
+              appstream-glib
+              desktop-file-utils
+              gtk4
+              libadwaita
+              meson
+              ninja
+              openssl
+              pkg-config
+              sqlite
+              wrapGAppsHook4
+            ];
+
+            meta = with pkgs.lib; {
+              description = ''
+                A podcast fetcher for the GNU/Linux Desktop
+                 (and mobile?) with support for discovery through
+                 podcastindex.org'';
+              homepage = "https://github.com/kylobytes/bolt";
+              license = licenses.gpl3;
+            };
           };
+          default = bolt;
+        };
 
-          buildInputs = with pkgs; [
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # rust deps
+            cargo
+            clippy
+            rust-analyzer
+            rustc
+            rustfmt
+
+            # glib/gtk deps
+            cairo
             gtk4
             libadwaita
-            sqlite
-          ];
 
-          nativeBuildInputs = with pkgs; [
-            cargo
-            rustPlatform.cargoSetupHook
-            rustc
-
+            # other deps
             appstream-glib
             desktop-file-utils
-            gtk4
-            libadwaita
+            fish
+            gcc
+            glib
+            libxml2
             meson
             ninja
             openssl
@@ -44,48 +79,7 @@
             wrapGAppsHook4
           ];
 
-          meta = with pkgs.lib; {
-            description = ''
-              A podcast fetcher for the GNU/Linux Desktop
-               (and mobile?) with support for discovery through
-               podcastindex.org'';
-            homepage = "https://github.com/kylobytes/bolt";
-            license = licenses.gpl3;
-          };
+          shellHook = "exec fish";
         };
-        default = bolt;
-      };
-
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          # rust deps
-          cargo
-          clippy
-          rust-analyzer
-          rustc
-          rustfmt
-
-          # glib/gtk deps
-          cairo
-          gtk4
-          libadwaita
-
-          # other deps
-          appstream-glib
-          desktop-file-utils
-          fish
-          gcc
-          glib
-          libxml2
-          meson
-          ninja
-          openssl
-          pkg-config
-          sqlite
-          wrapGAppsHook4
-        ];
-
-        shellHook = "exec fish";
-      };
-    });
+      });
 }
