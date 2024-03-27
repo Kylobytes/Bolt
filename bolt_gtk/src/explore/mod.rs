8cd17933c87bcd172cbd1667a13db152cdabc8ad
Kent Delante <leftybournes@pm.me>
2024-03-27 16:15:54 +0800

    Add preview widget

--- bolt_gtk/src/explore/mod.rs
+++ bolt_gtk/src/explore/mod.rs
@@ -1,3 +1,4 @@
 pub mod card;
 pub mod card_data;
+pub mod preview;
 pub mod view;
---
+++ bolt_gtk/src/explore/preview.rs
@@ -0,0 +1,55 @@
+/* preview.rs
+ *
+ * Copyright 2024 Kent Delante
+ *
+ * This file is part of Bolt.
+ *
+ * Bolt is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bolt is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bolt. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use adw::subclass::prelude::*;
+use gtk::{gio, glib};
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Default, gtk::CompositeTemplate)]
+    #[template(resource = "/com/kylobytes/Bolt/gtk/explore/preview.ui")]
+    pub struct Preview {}
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for Preview {
+        const NAME: &'static str = "Preview";
+        type Type = super::Preview;
+        type ParentType = adw::Bin;
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+
+    impl ObjectImpl for Preview {}
+    impl WidgetImpl for Preview {}
+    impl BinImpl for Preview {}
+}
+
+glib::wrapper! {
+    pub struct Preview(ObjectSubclass<imp::Preview>)
+        @extends gtk::Widget, adw::Bin,
+    @implements gio::ActionGroup, gio::ActionMap;
+}
