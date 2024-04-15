08e9d8839bc0ecfebed668597e09a62ea4ee1219
Kent Delante <leftybournes@pm.me>
2024-04-15 11:57:16 +0800

    Remove config.rs

--- src/config.rs
+++
@@ -1,8 +0,0 @@
-pub static VERSION: &str = "0.1.0";
-pub static GETTEXT_PACKAGE: &str = "bolt";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/bolt";
-pub static USER_AGENT: &str = "Bolt/1.0";
-pub static BASE_URL: &str = "https://api.podcastindex.org/api/1.0";
-pub static API_KEY: &str = "";
-pub static API_SECRET: &str = "";
